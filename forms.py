38f20f503163feba1cc2e30a04d01000d37a47e7
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-05-11 23:29:55 +0300

    updated forms.py file

--- forms.py
+++ forms.py
@@ -10,7 +10,7 @@ class RegistrationForm(FlaskForm):
       submit = SubmitField('Sign Up')
 
 
-class RegistrationForm(FlaskForm):
+class LoginForm(FlaskForm):
       email = StringField('Email', validators=[DataRequired(), Email()])
       password = PasswordField('Password', validators=[DataRequired()])
       remember = BooleanField('Remember Me')
