9d05d777d17dc49a3b20bfe0672a9b078a9deb88
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-05-13 19:05:14 +0300

    Added pagination on the index page displaying pitches

--- app/views.py
+++ app/views.py
@@ -11,7 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route('/')
 @app.route('/home')
 def index():
-      pitches = Pitch.query.all()
+      page = request.args.get('page', 1, type=int)
+      pitches = Pitch.query.order_by(Pitch.date_posted.desc()).paginate(page=page, per_page=6)
       return render_template('index.html', pitches=pitches)
 
 
